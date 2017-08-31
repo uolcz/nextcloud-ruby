464899343e3ef214f99351b5766e0c85a061aa8e
Andrej Antas <andrej@antas.cz>
2017-08-31 23:11:20 +0200

    Correct rubocop

--- nextcloud-ruby.gemspec
+++ nextcloud-ruby.gemspec
@@ -1,38 +1,39 @@
 # coding: utf-8
-lib = File.expand_path("../lib", __FILE__)
+
+lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "nextcloud/ruby/version"
+require 'nextcloud/ruby/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "nextcloud-ruby"
+  spec.name          = 'nextcloud-ruby'
   spec.version       = Nextcloud::Ruby::VERSION
-  spec.authors       = ["Andrej Antas"]
-  spec.email         = ["andrej@antas.cz"]
+  spec.authors       = ['Andrej Antas']
+  spec.email         = ['andrej@antas.cz']
 
-  spec.summary       = %q{Wrapper for nextcloud XML API}
-  spec.description   = %q{Wrapper for nextcloud XML API - folder structure creation, etc...}
-  spec.homepage      = "https://github.com/ucetnictvi-on-line/nextcloud-ruby"
-  spec.license       = "MIT"
+  spec.summary       = 'Wrapper for nextcloud XML API'
+  spec.description   = 'Wrapper for nextcloud XML API - folder structure creation, etc...'
+  spec.homepage      = 'https://github.com/ucetnictvi-on-line/nextcloud-ruby'
+  spec.license       = 'MIT'
 
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "https://rubygems.org"
+    spec.metadata['allowed_push_host'] = 'https://rubygems.org'
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against ' \
+      'public gem pushes.'
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+  spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
   spec.add_runtime_dependency 'net_dav', '~> 0.5'
 
-  spec.add_development_dependency "bundler", "~> 1.15"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.15'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'pry', '~> 0.10'
   spec.add_development_dependency 'rubocop', '~> 0.49'
   spec.add_development_dependency 'vcr', '~> 3.0'
--- spec/nextcloud/ruby/configuration_spec.rb
+++ spec/nextcloud/ruby/configuration_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe Nextcloud::Ruby::Configuration do
 
   context 'with configuration block' do
     it 'returns the correct dav_url' do
-      expect(Nextcloud::Ruby.configuration.dav_url).to eq(URI('https://example.com'))
+      expect(Nextcloud::Ruby.configuration.dav_url)
+        .to eq(URI('https://example.com'))
     end
 
     it 'returns the correct username' do
@@ -25,7 +26,8 @@ RSpec.describe Nextcloud::Ruby::Configuration do
     end
 
     it 'returns the correct dorectories' do
-      expect(Nextcloud::Ruby.configuration.directories).to match_array(['a/b/c', 'd/e/f'])
+      expect(Nextcloud::Ruby.configuration.directories)
+        .to match_array(['a/b/c', 'd/e/f'])
     end
   end
 
@@ -35,34 +37,44 @@ RSpec.describe Nextcloud::Ruby::Configuration do
     end
 
     it 'raises a configuration error for dav_url' do
-      expect { Nextcloud::Ruby.configuration.dav_url }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.dav_url }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
     end
 
     it 'raises a configuration error for username' do
-      expect { Nextcloud::Ruby.configuration.username }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.username }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
     end
 
     it 'raises a configuration error for password' do
-      expect { Nextcloud::Ruby.configuration.password }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.password }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
     end
 
     it 'raises a configuration error for directories' do
-      expect { Nextcloud::Ruby.configuration.directories }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.directories }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
     end
   end
 
   context '#reset' do
     it 'resets configured values' do
-      expect(Nextcloud::Ruby.configuration.dav_url).to eq(URI('https://example.com'))
+      expect(Nextcloud::Ruby.configuration.dav_url)
+        .to eq(URI('https://example.com'))
       expect(Nextcloud::Ruby.configuration.username).to eq('jon')
       expect(Nextcloud::Ruby.configuration.password).to eq('Secret')
-      expect(Nextcloud::Ruby.configuration.directories).to match_array(['a/b/c', 'd/e/f'])
+      expect(Nextcloud::Ruby.configuration.directories)
+        .to match_array(['a/b/c', 'd/e/f'])
 
       Nextcloud::Ruby.reset
-      expect { Nextcloud::Ruby.configuration.dav_url }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
-      expect { Nextcloud::Ruby.configuration.username }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
-      expect { Nextcloud::Ruby.configuration.password }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
-      expect { Nextcloud::Ruby.configuration.directories }.to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.dav_url }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.username }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.password }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
+      expect { Nextcloud::Ruby.configuration.directories }
+        .to raise_error(Nextcloud::Ruby::Errors::ConfigNotSet)
     end
   end
 end
